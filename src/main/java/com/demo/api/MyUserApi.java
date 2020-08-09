fa10b49a71c72312872662cf65b839e3df3e72fc
bale1992 <lx13478628642@163.com>
2020-08-09 10:07:25 +0800

    第一次提交

---
+++ src/main/java/com/demo/api/MyUserApi.java
@@ -0,0 +1,13 @@
+package com.demo.api;
+    import com.demo.model.User;
+import javax.servlet.ServletException;
+
+public interface MyUserApi {
+    User getUserById(Integer id) throws ServletException;
+
+    void deleteUserById(Integer id) throws ServletException;
+
+    void createUser(User user) throws ServletException;
+
+    void modifyUser(User user) throws ServletException;
+}
